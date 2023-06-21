7eea5c53eb35d9f612d09eb8f3d13004eb6b0ef6
jayval254 <valariekimuge1@gmail.com>
2023-06-21 20:27:39 +0000

    update

--- models/base_model.py
+++ models/base_model.py
@@ -42,3 +42,7 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        """Delete self from storage """
+        storage.delete(self)
