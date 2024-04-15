be2530abbf34030f18cb8c0c6673a041c6e25956
Valarie <valariekimuge1@gmail.com>
2024-04-15 19:19:41 +0000

    web flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script to start a Flask web application """
+""" Script to start a Flask web application-2 commands """
 
 from flask import Flask
 
@@ -12,6 +12,11 @@ def hello_world():
     """ Returns some text. """
     return 'Hello HBNB!'
 
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """ Return other text. """
+    return 'HBNB'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
